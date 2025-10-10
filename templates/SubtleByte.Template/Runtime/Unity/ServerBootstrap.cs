268cc57f34cdc227583ae09f338cfe516919bad4
Alwaleed Alshgnoubi <141871752+waleed199c@users.noreply.github.com>
2025-10-10 17:33:26 +0300

    Add performance-first SubtleByte template

---
+++ templates/SubtleByte.Template/Runtime/Unity/ServerBootstrap.cs
@@ -0,0 +1,52 @@
+using System;
+using BepInEx.Logging;
+using UnityEngine;
+using VeinWares.SubtleByte.Template.Infrastructure;
+
+namespace VeinWares.SubtleByte.Template.Runtime.Unity;
+
+public sealed class ServerBootstrap : IDisposable
+{
+    private readonly ManualLogSource _log;
+    private readonly ModuleHost _host;
+    private readonly GameObject _root;
+    private readonly ServerBehaviour _behaviour;
+
+    private ServerBootstrap(ModuleHost host, GameObject root, ServerBehaviour behaviour, ManualLogSource log)
+    {
+        _host = host;
+        _root = root;
+        _behaviour = behaviour;
+        _log = log;
+    }
+
+    public static ServerBootstrap Start(ModuleHost host, ManualLogSource log)
+    {
+        var go = new GameObject("SubtleByte.ModuleHost");
+        UnityEngine.Object.DontDestroyOnLoad(go);
+        var behaviour = go.AddComponent<ServerBehaviour>();
+        behaviour.Bind(host);
+        log.LogDebug("ServerBootstrap created persistent host GameObject.");
+        return new ServerBootstrap(host, go, behaviour, log);
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            if (_behaviour != null)
+            {
+                _behaviour.enabled = false;
+            }
+
+            if (_root != null)
+            {
+                UnityEngine.Object.Destroy(_root);
+            }
+        }
+        catch (Exception ex)
+        {
+            _log.LogWarning($"Failed to clean up ServerBootstrap GameObject: {ex}");
+        }
+    }
+}
