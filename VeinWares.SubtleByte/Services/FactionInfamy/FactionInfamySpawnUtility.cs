973c3b111c2ece112fa68e7377bf5dfc83b70f99
Alwaleed Alshgnoubi <141871752+waleed199c@users.noreply.github.com>
2025-10-20 22:20:18 +0300

    Cap ambush level advantage at ten

--- VeinWares.SubtleByte/Services/FactionInfamy/FactionInfamySpawnUtility.cs
+++ VeinWares.SubtleByte/Services/FactionInfamy/FactionInfamySpawnUtility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using ProjectM;
 using Stunlock.Core;
 using Unity.Entities;
@@ -9,55 +10,27 @@ namespace VeinWares.SubtleByte.Services.FactionInfamy;
 internal static class FactionInfamySpawnUtility
 {
     private static readonly Entity PlaceholderEntity = new();
+    private static readonly ConcurrentDictionary<int, PendingSpawnCallback> PendingCallbacks = new();
+    private static int _markerSequence = 100_000;
 
-    public static float EncodeLifetime(int lifetimeSeconds, int level, SpawnFaction faction)
+    public static bool HasPendingCallbacks => !PendingCallbacks.IsEmpty;
+
+    public static int SpawnUnit(
+        PrefabGUID prefab,
+        float3 position,
+        int count,
+        float minRange,
+        float maxRange,
+        float lifetime,
+        Action<EntityManager, Entity, int, float>? preFinalize = null)
     {
-        lifetimeSeconds = Math.Clamp(lifetimeSeconds / 10 * 10, 10, 990);
-        var factionDigit = Math.Clamp((int)faction, 0, 9);
-        level = Math.Clamp(level, 1, 99);
+        var marker = System.Threading.Interlocked.Increment(ref _markerSequence);
 
-        var factionComponent = factionDigit;
-        var levelComponent = level / 100f;
-        var checksumComponent = level / 10000f;
-
-        return lifetimeSeconds + factionComponent + levelComponent + checksumComponent;
-    }
-
-    public static bool TryDecodeLifetime(float encodedLifetime, out int level, out SpawnFaction faction)
-    {
-        var factionDigit = (int)(encodedLifetime % 10);
-        faction = Enum.IsDefined(typeof(SpawnFaction), factionDigit)
-            ? (SpawnFaction)factionDigit
-            : SpawnFaction.Default;
-
-        var levelSection = (encodedLifetime % 1) * 100;
-        level = (int)levelSection;
-
-        if (encodedLifetime > 1000 || level <= 0)
+        if (preFinalize != null)
         {
-            return false;
+            PendingCallbacks[marker] = new PendingSpawnCallback(count, lifetime, preFinalize);
         }
 
-        var checksumSection = (int)Math.Round((levelSection % 1) * 100);
-        if (checksumSection != level)
-        {
-            switch (level)
-            {
-                case 15:
-                case 40:
-                    checksumSection -= 1;
-                    break;
-                case 54:
-                    checksumSection += 1;
-                    break;
-            }
-        }
-
-        return checksumSection == level;
-    }
-
-    public static void SpawnUnit(PrefabGUID prefab, float3 position, int count, float minRange, float maxRange, float lifetime)
-    {
         Core.Server.GetExistingSystemManaged<UnitSpawnerUpdateSystem>().SpawnUnit(
             PlaceholderEntity,
             prefab,
@@ -65,13 +38,62 @@ internal static class FactionInfamySpawnUtility
             count,
             minRange,
             maxRange,
-            lifetime);
-    }
-}
+            marker);
 
-internal enum SpawnFaction
-{
-    Default = 0,
-    VampireHunters = 1,
-    WantedUnit = 2
+        return marker;
+    }
+
+    public static bool TryExecuteSpawnCallback(EntityManager entityManager, Entity entity, float lifetime)
+    {
+        var marker = (int)Math.Round(lifetime);
+        if (!PendingCallbacks.TryGetValue(marker, out var callback))
+        {
+            return false;
+        }
+
+        try
+        {
+            callback.Invoke(entityManager, entity, marker);
+        }
+        catch
+        {
+            // Suppress any callback errors to avoid breaking spawn flow.
+        }
+
+        if (callback.Decrement() <= 0)
+        {
+            PendingCallbacks.TryRemove(marker, out _);
+        }
+
+        return true;
+    }
+
+    public static void CancelSpawnCallback(int marker)
+    {
+        PendingCallbacks.TryRemove(marker, out _);
+    }
+
+    private sealed class PendingSpawnCallback
+    {
+        private int _remaining;
+        private readonly float _lifetime;
+        private readonly Action<EntityManager, Entity, int, float> _callback;
+
+        public PendingSpawnCallback(int remaining, float lifetime, Action<EntityManager, Entity, int, float> callback)
+        {
+            _remaining = Math.Max(1, remaining);
+            _lifetime = lifetime;
+            _callback = callback;
+        }
+
+        public int Decrement()
+        {
+            return System.Threading.Interlocked.Decrement(ref _remaining);
+        }
+
+        public void Invoke(EntityManager entityManager, Entity entity, int marker)
+        {
+            _callback(entityManager, entity, marker, _lifetime);
+        }
+    }
 }
