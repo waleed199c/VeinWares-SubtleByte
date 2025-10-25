203be2dcdaa749fb9db4bfa068605d039cec60da
Alwaleed Alshgnoubi <141871752+waleed199c@users.noreply.github.com>
2025-10-25 20:19:07 +0300

    Ensure module host advances when delta time is zero

--- VeinWares.SubtleByte/Runtime/Unity/ModuleHostBehaviour.cs
+++ VeinWares.SubtleByte/Runtime/Unity/ModuleHostBehaviour.cs
@@ -9,9 +9,25 @@ public sealed class ModuleHostBehaviour : MonoBehaviour
 {
     internal static Action<float>? TickHandler { get; set; }
 
+    private float _lastUpdateTimestamp;
+
+    private void Awake()
+    {
+        _lastUpdateTimestamp = Time.realtimeSinceStartup;
+    }
+
     private void Update()
     {
-        TickHandler?.Invoke(Time.deltaTime);
+        var now = Time.realtimeSinceStartup;
+        var delta = now - _lastUpdateTimestamp;
+        _lastUpdateTimestamp = now;
+
+        if (delta <= 0f)
+        {
+            delta = Time.deltaTime > 0f ? Time.deltaTime : Time.fixedDeltaTime;
+        }
+
+        TickHandler?.Invoke(delta);
     }
 
     private void OnDestroy()
